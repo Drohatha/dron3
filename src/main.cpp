1dd7ff5a355bf7673d9c29efe2cc1c985e1b97fa
drohatha <chrisabr@stud.ntnu.no>
2018-02-14 13:00:29 +0100

    Add yaw rate

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ ros::Publisher pub_setpoint;
 mavros_msgs::PositionTarget setpoint;
 
 uint16_t velocity_control = IGNORE_PX | IGNORE_PY | IGNORE_PZ |
-	                    IGNORE_AFX | IGNORE_AFY | IGNORE_AFZ | 
-	                    IGNORE_YAW_RATE;
+	                    IGNORE_AFX | IGNORE_AFY | IGNORE_AFZ;// | 
+	                    //IGNORE_YAW_RATE;
 
 
 
@@ -20,14 +20,13 @@ void joystickCallback(const geometry_msgs::Twist& input){
 
 	setpoint.coordinate_frame = mavros_msgs::PositionTarget::FRAME_LOCAL_NED;
 	setpoint.type_mask = velocity_control;
-	setpoint.position.x = input.linear.x;
-	setpoint.position.y = input.linear.y;
-	setpoint.position.z = input.linear.z;				
+	setpoint.velocity.x = input.linear.x;
+	setpoint.velocity.y = input.linear.y;
+	setpoint.velocity.z = input.linear.z;				
 	setpoint.header.frame_id = "fcu";
 	setpoint.yaw = -PI_HALF;
-	pub_setpoint.publish(setpoint); 
-
-
+	setpoint.yaw_rate = input.angular.z; 
+	pub_setpoint.publish(setpoint);
 }
 
 

0e1b0ebf559d1c0a6cf2be51635af2175a03b854
drohatha <chrisabr@stud.ntnu.no>
2018-02-14 15:00:05 +0100

    Add attitude control

--- src/attitude_control.cpp
+++ src/attitude_control.cpp
@@ -2,31 +2,33 @@
 #include <geometry_msgs/Twist.h>
 #include <mavros_msgs/PositionTarget.h>
 #include <msgs_definitions/setpoint_msg_defines.h>
-
+#include <tf/transform_datatypes.h>
 
 constexpr float PI_HALF = 1.570796; 
 
 ros::Publisher pub_setpoint;
 
-mavros_msgs::PositionTarget setpoint;
 
-uint16_t velocity_control = IGNORE_PX | IGNORE_PY | IGNORE_PZ |
-	                    IGNORE_AFX | IGNORE_AFY | IGNORE_AFZ  |
-	                    IGNORE_YAW;// | 
-	                    //IGNORE_YAW_RATE;
+mavros_msgs::AttitudeTarget setpoint;
+
+uint16_t attitude_control = 
 
 
 
 void joystickCallback(const geometry_msgs::Twist& input){
 
-	setpoint.coordinate_frame = mavros_msgs::PositionTarget::FRAME_LOCAL_NED;
+	tf::Quaternion q = tf::createQuaternionFromRPY(input.angular.x, input.angular.y, input.angular.z);
+	q.normalize(); 
+
+
 	setpoint.type_mask = velocity_control;
-	setpoint.velocity.x = input.linear.x;
-	setpoint.velocity.y = input.linear.y;
-	setpoint.velocity.z = input.linear.z;				
+	setpoint.orientation.x = ?
+	setpoint.orientation.y = ?
+	setpoint.orientation.z = ?
+	setpoint.orientation.w = ? 
+	setpoint.type_mask = velocity_control;
 	setpoint.header.frame_id = "fcu";
-	setpoint.yaw = -PI_HALF;
-	setpoint.yaw_rate = input.angular.z; 
+
 	pub_setpoint.publish(setpoint);
 }
 

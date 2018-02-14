35739302961cf06e25db309724677b1fd65d7ac4
drohatha <chrisabr@stud.ntnu.no>
2018-02-14 12:27:41 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <mavros_msgs/PositionTarget.h>
+#include <msgs_definitions/setpoint_msg_defines.h>
+
+
+constexpr float PI_HALF = 1.570796; 
+
+ros::Publisher pub_setpoint;
+
+mavros_msgs::PositionTarget setpoint;
+
+uint16_t velocity_control = IGNORE_PX | IGNORE_PY | IGNORE_PZ |
+	                    IGNORE_AFX | IGNORE_AFY | IGNORE_AFZ | 
+	                    IGNORE_YAW_RATE;
+
+
+
+void joystickCallback(const geometry_msgs::Twist& input){
+
+	setpoint.coordinate_frame = mavros_msgs::PositionTarget::FRAME_LOCAL_NED;
+	setpoint.type_mask = velocity_control;
+	setpoint.position.x = input.linear.x;
+	setpoint.position.y = input.linear.y;
+	setpoint.position.z = input.linear.z;				
+	setpoint.header.frame_id = "fcu";
+	setpoint.yaw = -PI_HALF;
+	pub_setpoint.publish(setpoint); 
+
+
+}
+
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "joystick_repeater"); 
+	ros::NodeHandle n;
+
+	ros::Subscriber sub_joystick = n.subscribe("spacenav/twist", 100, joystickCallback); 
+
+	pub_setpoint = n.advertise<mavros_msgs::PositionTarget>("/mavros/setpoint_raw/local", 100);
+
+	ros::spin();
+
+	return 0;
+}

b8c4591325d9f80b59ce3cff423f1e45d6469c7a
drohatha <chrisabr@stud.ntnu.no>
2018-02-21 09:11:23 +0100

    Add msgs attitude defines

---
+++ include/msgs_definitions/setpoint_msg_attitude_defines.h
@@ -0,0 +1,11 @@
+#ifndef SETPOINT_MSG_ATTITUDE_DEFINES
+#define SETPOINT_MSG_ATTITUDE_DEFINES
+
+#define IGNORE_ROLL_RATE (1 << 0)
+#define IGNORE_PITCH_RATE (1 << 1)
+#define IGNORE_YAW_RATE (1 << 2)
+#define IGNORE_THRUST (1<< 6)
+#define IGNORE_ATTITUDE (1 << 7)
+
+
+#endif
